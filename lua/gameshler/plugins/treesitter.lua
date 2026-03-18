6f2efa461c27f1c723b36d1d9cc3007c147c4a92
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-03-18 18:20:20 +0200

    Update treesitter.lua

--- lua/gameshler/plugins/treesitter.lua
+++ lua/gameshler/plugins/treesitter.lua
@@ -1,5 +1,74 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"vimdoc",
+					"javascript",
+					"typescript",
+					"c",
+					"lua",
+					"rust",
+					"jsdoc",
+					"bash",
+				},
+
+				sync_install = false,
+
+				auto_install = true,
+
+				indent = {
+					enable = true,
+				},
+
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						if lang == "html" then
+							print("disabled")
+							return true
+						end
+
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							vim.notify(
+								"File larger than 100KB treesitter disabled for performance",
+								vim.log.levels.WARN,
+								{ title = "Treesitter" }
+							)
+							return true
+						end
+					end,
+
+					additional_vim_regex_highlighting = { "markdown" },
+				},
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		after = "nvim-treesitter",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true,
+				multiwindow = false,
+				max_lines = 0,
+				min_window_height = 0,
+				line_numbers = true,
+				multiline_threshold = 20,
+				trim_scope = "outer",
+				mode = "cursor",
+				separator = nil,
+				zindex = 20,
+				on_attach = nil,
+			})
+		end,
+	},
 }
