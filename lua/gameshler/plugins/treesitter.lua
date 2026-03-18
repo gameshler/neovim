c75de1bcda82731469979a39cdee9865a3f08b1e
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-03-18 19:30:44 +0200

    Update treesitter.lua

--- lua/gameshler/plugins/treesitter.lua
+++ lua/gameshler/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		branch = "master",
+		lazy = false,
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
